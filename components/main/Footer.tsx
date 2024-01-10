c4a583d9466935d2b545f3f919df39de82032acb
AmanBhardwaj-Git <dayabhardwaj10@gmail.com>
2024-01-10 20:39:01 +0530

    last count

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -7,65 +7,50 @@ import {
   RxLinkedinLogo,
 } from "react-icons/rx";
 
-import { FaYoutube } from "react-icons/fa";
+import { FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
-        <div className="w-full flex flex-col items-center justify-center m-auto">
-            <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
-                
+      <div className="w-full flex flex-col items-center justify-center m-auto">
+        <div className="w-full h-full flex flex-row items-center justify-around flex-wrap z-20">
+          <div className="w-full md:w-[500px] h-auto flex flex-col flex-wrap items-center justify-center">
+            <div className="font-bold text-[16px]">Social Media</div>
+            <div className="w-full flex gap-5 flex-wrap px-5 justify-center items-center">
 
-                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">Community</div>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <FaYoutube />
-                        <span className="text-[15px] ml-[6px]">Youtube</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxGithubLogo />
-                        <span className="text-[15px] ml-[6px]">Github</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxDiscordLogo />
-                        <span className="text-[15px] ml-[6px]">Discord</span>    
-                    </p>
-                </div>
-                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">Social Media</div>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <FaYoutube />
-                        <span className="text-[15px] ml-[6px]">Instagram</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxGithubLogo />
-                        <span className="text-[15px] ml-[6px]">Twitter</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxDiscordLogo />
-                        <span className="text-[15px] ml-[6px]">Linkedin</span>    
-                    </p>
-                </div>
-                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">About</div>
-               
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                      
-                        <span className="text-[15px] ml-[6px]">Learning about me</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                  
-                        <span className="text-[15px] ml-[6px]">dayalbhardwaj10@gmail.com</span>    
-                    </p>
-                </div>
-            </div>
-
-            <div className="mb-[20px] text-[15px] text-center">
-                &copy; ABSITE. All rights reserved
-            </div>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <FaInstagram />
+              <span className="text-[15px] ml-[6px]">Instagram</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <RxGithubLogo />
+              <span className="text-[15px] ml-[6px]">Github</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <RxTwitterLogo />
+              <span className="text-[15px] ml-[6px]">Twitter</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <RxLinkedinLogo />
+              <span className="text-[15px] ml-[6px]">Linkedin</span>
+            </p>
+          </div>
+          </div>
+          <div className="w-[200px] h-auto flex flex-col items-center justify-start">
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <span className="text-[15px] ml-[6px]">
+                dayalbhardwaj10@gmail.com
+              </span>
+            </p>
+          </div>
         </div>
-    </div>
-  )
-}
 
-export default Footer
+        <div className="mb-[20px] text-[15px] text-center">
+          &copy; ABSITE. All rights reserved
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -4,11 +4,11 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto md:px-[10px]">
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 md:px-10">
+      <div className="w-full h-full flex flex-row items-center md:justify-between m-auto md:px-[10px]">
         <a
           href="#about-me"
-          className="h-auto w-auto flex flex-row items-center "
+          className="h-auto w-full flex flex-row items-center justify-center md:justify-normal"
         >
           <Image
             src="/NavLogo.svg"
@@ -18,12 +18,12 @@ const Navbar = () => {
             className="cursor-pointer hover:animate-spin transition-all duration-5 -ml-5"
           />
 
-          <span className="tracking-wider font-bold ml-[0.5vw] hidden md:block text-gray-300">
+          <span className="tracking-wider font-bold  hidden md:block text-gray-300">
             ABSITE
           </span>
         </a>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
+        <div className="min-w-[250px] md:w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
             <a href="#about-me" className="cursor-pointer">
               About
