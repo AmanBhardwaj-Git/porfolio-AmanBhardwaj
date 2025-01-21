88ffe63dc04de5cd722cfad82bae00de9c624dd9
AmanBhardwaj-Git <dayabhardwaj10@gmail.com>
2025-01-21 22:04:11 +0530

    Update layout metadata and add structured data for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Aman Bhardwaj portfolio",
-  description: "Animated Portfolio",
+  title: "Aman Bhardwaj - Portfolio | Next.js Developer",
+  description: "Welcome to the portfolio of Aman Bhardwaj, showcasing projects built with Next.js and other technologies.",
 };
 
 export default function RootLayout({
@@ -19,9 +19,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+     
       <body
         className={`${inter.className} bg-[#020209] overflow-y-scroll overflow-x-hidden`}
       >
+        <script type="application/ld+json" suppressHydrationWarning>
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Person",
+            "name": "Aman Bhardwaj",
+            "url": "https://porfolio-animated.vercel.app/",
+   
+            "sameAs": [
+              "https://x.com/i/flow/login?redirect_after_login=%2FbhardWAman",
+              "https://github.com/AmanBhardwaj-Git"
+            ]
+          })}
+        </script>
         <StarsCanvas />
         <Navbar />
         {children}
